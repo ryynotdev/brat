bf9a234073f1328598182dc13fe325df89fda5b6
ryynotdev <alfarisy1120@gmail.com>
2025-09-02 21:17:30 +0700

    Update brat.js

--- api/brat.js
+++ api/brat.js
@@ -2,9 +2,9 @@ export default async function handler(req, res) {
   const { text } = req.query;
 
   const svg = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512">
-      <rect width="100%" height="100%" fill="#ff69b4"/>
-      <text x="50%" y="50%" font-size="40" text-anchor="middle" fill="white" dy=".3em" font-family="Arial, sans-serif">
+    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+      <rect width="512" height="512" fill="white"/>
+      <text x="50%" y="50%" font-size="48" text-anchor="middle" fill="black" dy=".3em" font-family="Arial, sans-serif">
         ${text || "Brat 💗"}
       </text>
     </svg>
